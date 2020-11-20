b5400844aba3d16bf66c233dacd36e5bb6dbd553
Changshijie-0318 <1580630512@qq.com>
2020-11-20 16:29:35 +0800

    Mapper

---
+++ src/main/java/com/tedu/mapper/Mapper.java
@@ -0,0 +1,11 @@
+package com.tedu.mapper;/**
+ * @ProjectName:    page 
+ * @Package:        com.tedu.mapper
+ * @ClassName:      Mapper
+ * @Author:     Chase
+ * @Description:  Mapper  
+ * @Date:    2020/11/20 16:28
+ * @Version:    1.0
+ */
+public interface Mapper {
+}
